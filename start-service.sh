9dbf99110105c854143005ad2648ef2e619900b0
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-06-28 04:38:59 +0000

    Update start-service.sh

--- start-service.sh
+++ start-service.sh
@@ -4,14 +4,14 @@
 sysctl -w vm.nr_hugepages=1408
 
 ## Old Ryzen-specific modprobe optimization script
-#modprobe msr
-#wrmsr -a 0xC0011022 0x510000
-#wrmsr -a 0xC001102b 0x1808cc16
-#wrmsr -a 0xC0011020 0
-#wrmsrsr -a 0xC0011021 0x40
+modprobe msr
+wrmsr -a 0xC0011022 0x510000
+wrmsr -a 0xC001102b 0x1808cc16
+wrmsr -a 0xC0011020 0
+wrmsrsr -a 0xC0011021 0x40
 
 ## Service-specific script for autorun
-XMRSTAK="/opt/xmr-stak/build/bin"
+XMRSTAK="/opt/xmr-stak-rx/build/bin"
 cd $XMRSTAK; $XMRSTAK/randomx_booster.sh; $XMRSTAK/xmr-stak-rx -c $XMRSTAK/config.txt --noTest;
 
 ## Old script that only seems to run manually
